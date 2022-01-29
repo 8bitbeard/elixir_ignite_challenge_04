0b666ffcc8880352bb376c2e4dd1332d4dbbd313
Wilton Souza <willsouzafilho@gmail.com>
2022-01-29 17:29:23 -0300

    Finishing challenge

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -47,6 +47,20 @@ defmodule GenReport do
     {:error, "Insira o nome de um arquivo"}
   end
 
+  def parallel_build(filenames) when not is_list(filenames) do
+    {:error, "Favor fornecer uma lista de arquivos!"}
+  end
+
+  def parallel_build(filenames) do
+    filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(empty_report(), fn {:ok, result}, report -> sum_reports(report, result) end)
+  end
+
+  def parallel_build() do
+    {:error, "Favor fornecer uma lista de arquivos!"}
+  end
+
   defp sum_values([name, hours, _day, month, year],
   %{"all_hours" => all_hours, "hours_per_month" => hours_per_month, "hours_per_year" => hours_per_year}) do
     all_hours = Map.put(all_hours, name, all_hours[name] + hours)
@@ -56,6 +70,23 @@ defmodule GenReport do
     hours_per_year = Map.put(hours_per_year, name, year_map)
 
     build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp sum_reports(%{"all_hours" => all_hours1, "hours_per_month" => hours_per_month1, "hours_per_year" => hours_per_year1},
+  %{"all_hours" => all_hours2, "hours_per_month" => hours_per_month2, "hours_per_year" => hours_per_year2}) do
+    all_hours = merge_maps(all_hours1, all_hours2)
+    hours_per_month = merge_inner_maps(hours_per_month1, hours_per_month2)
+    hours_per_year = merge_inner_maps(hours_per_year1, hours_per_year2)
+
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _k, v1, v2 -> v1 + v2 end)
+  end
+
+  defp merge_inner_maps(map1, map2) do
+    Map.merge(map1, map2, fn _k, v1, v2 -> merge_maps(v1, v2) end)
   end
 
   def empty_report do
--- test/gen_report_test.exs
+++ test/gen_report_test.exs
@@ -5,6 +5,7 @@ defmodule GenReportTest do
   alias GenReport.Support.ReportFixture
 
   @file_name "gen_report.csv"
+  @file_list ["part_1.csv", "part_2.csv", "part_3.csv"]
 
   describe "build/1" do
     test "When passing file name return a report" do
@@ -17,6 +18,26 @@ defmodule GenReportTest do
       response = GenReport.build()
 
       assert response == {:error, "Insira o nome de um arquivo"}
+    end
+  end
+
+  describe "parallel_build/1" do
+    test "When passing a file list return a report" do
+      response= GenReport.parallel_build(@file_list)
+
+      assert response == ReportFixture.build()
+    end
+
+    test "When passing a single file, returns an error" do
+      response = GenReport.parallel_build(@file_name)
+
+      assert response == {:error, "Favor fornecer uma lista de arquivos!"}
+    end
+
+    test "When no list of filenames is given, returns an error" do
+      response = GenReport.parallel_build()
+
+      assert response == {:error, "Favor fornecer uma lista de arquivos!"}
     end
   end
 end
